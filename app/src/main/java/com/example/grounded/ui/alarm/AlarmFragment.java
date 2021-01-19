c504604df04e4ac41658e464d3b838da255f03b8
Matthew Nosworthy <mnosworthy007@gmail.com>
2021-01-19 15:08:04 +0000

    added images to the nav bars

---
+++ app/src/main/java/com/example/grounded/ui/alarm/AlarmFragment.java
@@ -0,0 +1,34 @@
+package com.example.grounded.ui.alarm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.grounded.R;
+
+public class AlarmFragment extends Fragment {
+
+    private AlarmViewModel alarmViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        alarmViewModel =
+                ViewModelProviders.of(this).get(AlarmViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_alarm, container, false);
+        final TextView textView = root.findViewById(R.id.text_alarm);
+        alarmViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
